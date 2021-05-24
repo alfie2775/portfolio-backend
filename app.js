bbc8d3f7ee478ac8a5dda640be41db40c793aef9
alfie2775 <nunetarun2001@gmail.com>
2021-05-24 11:57:01 +0530

    added: smtp

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 const express = require("express");
+const mailer = require("express-mailer");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { MongoClient } = require("mongodb");
 dotenv.config();
-var db;
 const app = express();
-const client = new MongoClient(process.env.MONGOURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).connect();
-
-client.then((client) => {
-  console.log("Connected to database");
-  db = client.db("Mails");
+mailer.extend(app, {
+  from: "no-reply@example.com",
+  host: "smtp.gmail.com",
+  secureConnection: true,
+  port: 465,
+  transportMethod: "SMTP",
+  auth: {
+    user: process.env.MAIL,
+    pass: process.env.MAILPASS,
+  },
 });
 app.use(cors());
 app.use(express.json());
+app.set("views", __dirname + "/views");
+app.set("view engine", "jade");
 app.post("/send-message", (req, res) => {
-  db.collection("mails")
-    .insertOne({ ...req.body })
-    .then(() => res.send({ success: true }))
-    .catch((err) => res.send({ success: false, err }));
+  app.mailer.send(
+    "email",
+    {
+      to: "nunetarun2001@gmail.com",
+      subject: "From Portfolio",
+      name: req.body.name,
+      email: req.body.email,
+      message: req.body.message,
+    },
+    (err) => {
+      if (err) res.send({ success: false, err });
+      else res.send({ success: true });
+    }
+  );
 });
 const PORT = process.env.PORT || 5000;
 
